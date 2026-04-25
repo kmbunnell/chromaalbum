d1f9189e5715d5d0d29e915216d84799bf5c56be
Kristina Bunnell <mrskbunnell@gmail.com>
2026-04-25 17:13:35 -0400

    Set up default colorscheme

---
+++ app/src/main/java/com/example/chromaalbum/ui/theme/DefaultColorSchemes.kt
@@ -0,0 +1,9 @@
+package com.example.chromaalbum.ui.theme
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+
+object DefaultColorSchemes {
+    val lightDefault = lightColorScheme(primary = Purple40)
+    val darkDefault = darkColorScheme(primary = Purple80)
+}
